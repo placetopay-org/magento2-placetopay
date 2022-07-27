51969a7183beff68eee38c335a01ce2333a3b12c
Alejandro-Castrillon-Ciro <alejandro.castrillon@evertecinc.com>
2022-07-27 09:10:35 -0500

    feat: add constants of payments

---
+++ Constants/PaymentStatus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PlacetoPay\Payments\Constants;
+
+class PaymentStatus
+{
+    public const PENDING_PAYMENT = 'pending_payment';
+    public const PENDING = 'pending';
+    public const CANCELED = 'canceled';
+    public const PROCESSING = 'processing';
+    public const APPROVED = 'APPROVED';
+    public const REJECTED = 'REJECTED';
+}
