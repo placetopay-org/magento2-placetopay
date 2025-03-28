4f307d0de57d21f26ac3a3afc1ea83211460e1ff
Freddie Gar <freddie.gar@outlook.com>
2025-03-28 18:26:31 -0500

    Updated CHANGELOG 1.12.2

--- Model/PaymentMethod.php
+++ Model/PaymentMethod.php
@@ -172,7 +172,7 @@ class PaymentMethod extends AbstractMethod
         $this->orderRepository = $orderRepository;
         $this->infoFactory = $infoFactory;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-        $this->version = '1.12.1';
+        $this->version = '1.12.2';
 
         $this->placetoPayPayment = new PlacetoPayPayment(
             $config,
