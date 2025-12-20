14d26592cdfcab3750c382ec73d9aba842c0e7db
Luis Felipe Garcia H <felipe.garciaherrera@evertecinc.com>
2025-12-20 11:49:34 -0500

    Delete test.php
    chore: remove unused file

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include 'vendor/autoload.php';
-
-echo
-\Banchile\Payments\Helper\ParseData::unmaskString('https://placetopay-static-prod-bucket.s3.us-east-2.amazonaws.com/banchile/logos/Logotipo_superior.png');
