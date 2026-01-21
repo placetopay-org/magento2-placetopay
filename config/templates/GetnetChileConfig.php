f22630e3a2738868a6a1f5a2ba586fdea05324a4
Alejandro Castrillon Ciro <alejandro.cciro@udea.edu.co>
2026-01-21 16:09:08 -0500

    feat: implement client configuration and generate whitelabel

---
+++ config/templates/GetnetChileConfig.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace PlacetoPay\Payments;
+
+use PlacetoPay\Payments\Model\Adminhtml\Source\Mode;
+
+abstract class CountryConfig
+{
+    public const CLIENT_ID = 'getnet_chile';
+    public const CLIENT = 'Getnet';
+    public const IMAGE = 'https://banco.santander.cl/uploads/000/029/870/0620f532-9fc9-4248-b99e-78bae9f13e1d/original/Logo_WebCheckout_Getnet.svg';
+    public const COUNTRY_CODE = 'CL';
+    public const COUNTRY_NAME = 'Chile';
+
+    public static function getEndpoints(): array
+    {
+        return [
+            Mode::DEVELOPMENT => 'https://checkout-co.placetopay.dev',
+            Mode::TEST => 'https://checkout.test.getnet.cl',
+            Mode::PRODUCTION => 'https://checkout.getnet.cl',
+        ];
+    }
+
+    public static function getClient(): array
+    {
+        return [
+            [
+                'value' => self::CLIENT,
+                'label' => __(self::CLIENT),
+            ],
+        ];
+    }
+}
+
---
+++ config/templates/PlacetopayEcuadorConfig.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PlacetoPay\Payments;
+
+use PlacetoPay\Payments\Model\Adminhtml\Source\Mode;
+
+abstract class CountryConfig 
+{
+    public const CLIENT_ID = 'placetopay_ecuador';
+    public const CLIENT = 'Placetopay';
+    public const IMAGE = 'https://static.placetopay.com/placetopay-logo.svg';
+    public const COUNTRY_CODE = 'EC';
+    public const COUNTRY_NAME = 'Ecuador';
+
+
+    public static function getEndpoints(): array
+    {
+        return [
+            Mode::DEVELOPMENT => 'https://dev.placetopay.ec/redirection',
+            Mode::TEST => 'https://checkout-test.placetopay.ec',
+            Mode::PRODUCTION => 'https://checkout.placetopay.ec',
+        ];
+    }
+
+    public static function getClient(): array
+    {
+        return [
+            [
+                'value' => self::CLIENT,
+                'label' => __(self::CLIENT),
+            ],
+        ];
+    }
+}
+
