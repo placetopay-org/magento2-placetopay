43c8e6fd004420cfe7a6708603c2ccf38e90792e
Freddie Gar <freddie.gar@outlook.com>
2026-02-06 19:49:30 -0500

    feat: remove unused constants, use new static functions

--- Constants/PathUrlRedirect.php
+++ Constants/PathUrlRedirect.php
@@ -6,20 +6,16 @@ use PlacetoPay\Payments\CountryConfig;
 
 abstract class PathUrlRedirect
 {
-    public const FAILURE = null; // Se inicializa dinámicamente
-    public const SUCCESSFUL = null; // Se inicializa dinámicamente
-    public const PENDING = null; // Se inicializa dinámicamente
-
     public static function getFailure(): string
     {
         return CountryConfig::CLIENT_ID . '/onepage/failure';
     }
-    
+
     public static function getSuccessful(): string
     {
         return CountryConfig::CLIENT_ID . '/onepage/success';
     }
-    
+
     public static function getPending(): string
     {
         return CountryConfig::CLIENT_ID . '/onepage/pending';
